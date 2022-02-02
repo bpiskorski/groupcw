8e503ea0c94edd5d3880204c66a2f3aef37c0042
Kristijan Segulja <40493610@live.napier.ac.uk>
2022-02-02 19:12:38 +0000

    Added Java App

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! This is a test message.");
+        System.out.println("Hello World! This is a test message!");
     }
 }
