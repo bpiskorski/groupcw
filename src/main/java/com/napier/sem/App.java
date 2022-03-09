2e02e91dab64ed6eeffe02ddf4c3d55c684d0af4
bpiskorski <87440108+bpiskorski@users.noreply.github.com>
2022-03-09 08:49:51 +0000

    dockerfile changes, working connection with database

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,21 +9,23 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
-
 public class App{
 
 
-
-    public static void main(String[] args)
-    {
-
-        System.out.println("Hello World! This is a test message!");
-
-       DbConnect con1 = new DbConnect();
-        con1.connect();
+    public static void main(String[] args) {
 
 
+        {
+
+            System.out.println("Hello World! This is a test message!");
+
+            DbConnect con1 = new DbConnect();
+            con1.connect();
+
+
+        }
     }
 }
+
 
 
--- src/main/java/com/napier/sem/DbConnect.java
+++ src/main/java/com/napier/sem/DbConnect.java
@@ -1,44 +1,58 @@
 package com.napier.sem;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ServiceLoader;
 
 public class DbConnect {
     static final String DB_URL = "jdbc:mysql://db:3306/world?useSSL=false";
     static final String USER = "root";
-    static final String PASS = "example";
+    static final String PASS = "root";
 
     public DbConnect() {
     }
 
     public void connect(){
-        Connection con = null;
+        Connection conn = null;
 
         System.out.println("Connecting to a selected database...");
 
-        try
-        {
-            // Load Database driver
+      /*     try
+            {
+                // Load Database driver
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                System.out.println("Could not load SQL driver");
+
+                 System.exit(-1);
+            }
+*/
+        // Connection to the database
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+            //Thread.sleep(30000);
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        } catch (Exception ex) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
-
-
-        // Connection to the database
 
         int retries = 100;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection(DB_URL,USER,PASS);
+                //Thread.sleep(30000);
 
+                // Connect to database
+                /*conn = DriverManager.getConnection(DB_URL,USER,PASS);*/
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306?"+
+                        "user=root&password=root");
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(10000);
