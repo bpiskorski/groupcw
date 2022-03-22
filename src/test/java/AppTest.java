7d1937ffdbfd6cd64a8b76a41fdce11bbd4413bc
Vitali Turkin <40532761@live.napier.ac.uk>
2022-03-22 13:30:42 +0000

    Country reports

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,28 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 0);
+    }
+
+    @Test
+    void countryrep()
+    {
+        app.countryrep();
+    }
+
+    @AfterAll
+    static void close(){
+        app.disconnect();
+    }
+}
