a66a9bb3541a7f7c0862863386900ab4e2e5c374
Pawel Stankiewicz <40539746@live.napier.ac.uk>
2022-03-22 14:09:45 +0000

    + jUnit + AppTest

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,18 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    static App app;
+
+    @BeforeAll      // creating an instance of App to work with
+    static void init() {
+        app = new App();
+    }
+}
