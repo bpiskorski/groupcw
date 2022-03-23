4f9ad7e21fe28ddead86f46e81cb2222bcab48e4
bpiskorski <87440108+bpiskorski@users.noreply.github.com>
2022-03-23 01:44:27 +0000

    Created a feature that produce reports of chinese speakers in a world

---
+++ src/test/java/com/napier/sem/LanguageDataTest.java
@@ -0,0 +1,49 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LanguageDataTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCountryCode() {
+    }
+
+    @Test
+    void setCountryCode() {
+    }
+
+    @Test
+    void getLanguage() {
+    }
+
+    @Test
+    void setLanguage() {
+    }
+
+    @Test
+    void isOfficial() {
+    }
+
+    @Test
+    void setOfficial() {
+    }
+
+    @Test
+    void getPercentage() {
+    }
+
+    @Test
+    void setPercentage() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
