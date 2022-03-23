2e8691d480e54822012e5859e6d10995b1bd9119
bpiskorski <87440108+bpiskorski@users.noreply.github.com>
2022-03-23 01:44:27 +0000

    Created a feature that produce reports of chinese speakers in a world

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,115 +8,50 @@ import java.io.FileWriter;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class App {
 
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
 
-    public static void main(String[] args) {
-        // Create new Application
-        App a = new App();
 
-        // Connect to database
-        a.connect("db:3306", 3000);
+    public static void main(String[] args) throws SQLException {
 
-        a.countryrep();// 1) Provide all countries in a world from largest population to smallest.
-                       //"select * from world.country order by Population desc;";
-                       // 2) Provide countries in a region from largest population to smallest
-                       //"select * from world.country Where Region = 'Eastern Asia' order by Population desc;";
-                       // 3) Provide counties from largest population to smallest in a continent
-                       //"select * from world.country Where Continent = 'Eastern Asia' order by Population desc;";
-                       // 4) The top N populated countries in a region where N is provided by the user.
-                       //"select * from world.country where Region ='Eastern Asia' order by Population desc limit 3;";
-                       // 5)The top N populated countries in a continent where N is provided by the user.
-                       //"select  * from world.country where Continent ='Asia' order by Population desc limit 3;";
-                       // 6)The top N populated countries in the world where N is provided by the user.
-                       //"select * from world.country order by Population desc limit 3;";
+        Languages first = new Languages();
+        System.out.println("----------------------------------------------");
 
-        // Disconnect from database
-        a.disconnect();
-    }
+        ArrayList<LanguageData> chinese = new ArrayList<>();
+        ArrayList<Country> countries = new ArrayList<>();
+        int chineseSpeakers=0;
+        chinese = first.chinese();
+        countries = first.countriesData();
+        for (LanguageData ld: chinese
+             ) {
+            System.out.println(ld.toString());
+          String x = ld.getCountryCode();
+          float i = ld.getPercentage()/100;
+            for (Country c: countries
+                 ) {if(c.getCode().equals(x)){
+                     int pop = c.getPopulation();
+                System.out.println(c.getPopulation());
+                     int res = (int) (pop*i);
+                chineseSpeakers += res;
+            }
 
-    /**
-     * Connect to the MySQL database.
-     * @param conString Use db:3306 for docker and localhost:33060 for local or Integration Tests
-     * @param i
-     */
-    public void connect(String conString, int delay) {
-        try {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
 
-        int retries = 10;
-        for (int i = 0; i < retries; ++i) {
-            System.out.println("Connecting to database...");
-            try {
-                // Wait a bit for db to start
-                Thread.sleep(delay);
-                // Connect to database
-                //Added allowPublicKeyRetrieval=true to get Integration Tests to work. Possibly due to accessing from another class?
-                con = DriverManager.getConnection("jdbc:mysql://" + conString + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
-                System.out.println("Thread interrupted? Should not happen.");
             }
         }
-    }
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect() {
-        if (con != null) {
-            try {
-                // Close connection
-                con.close();
-            } catch (Exception e) {
-                System.out.println("Error closing connection to database");
-            }
-        }
-    }
+        System.out.println("Chinese speakers in a world: " + chineseSpeakers);
 
-    public void countryrep() {
-        System.out.println("Creating the report...");
-        StringBuilder sb  = new StringBuilder();
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String sql ="select * from world.country Where Continent ='Europe' order by Population desc;";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(sql);
-            // Return  if valid.
-            // Check one is returned
-            while (rset.next()) {
-                String code = rset.getString("code");
-                String name = rset.getString("name");
-                String continent = rset.getString("continent");
-                String region = rset.getString("region");
-                Integer population = rset.getInt("population");
-                Integer capital = rset.getInt("capital");
-                Country country = new Country(code, name, continent, region, population, capital);
-                sb.append(country.toString() + "\r\n");
-            } new File("./reports/").mkdir();
-            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("./reports/task03.txt")));
-            writer.write(sb.toString());
-            writer.close();
-            System.out.println("Report created look in a reports folder!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get country details");
-            return;
+
+
+
+            }
+
         }
-    }
-}
+
+
+
+
